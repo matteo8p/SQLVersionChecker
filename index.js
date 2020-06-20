242cf19e4330f098d1c9b3389e54b069687c72c3
matteo8p <matt8p@outlook.com>
2020-06-20 18:34:17 -0400

    run

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 const nameToGreet = core.getInput('name');
 
-if(nameToGreet.valueOf() === "Matthew".valueOf())
-{
-  console.log("Hey there, You submitted Matthew");
-}else
-{
-  core.setFailed("Error!");
-}
+console.log("Hey there " + nameToGreet);
+
+fs.readdir("./", function(err, items) {
+  console.log(items);
+
+  for (var i=0; i<items.length; i++) {
+    console.log(items[i]);
+  }
+});
+
+
 
