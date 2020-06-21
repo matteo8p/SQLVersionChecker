84c27a57c699708209a48db5b4db72b431380abf
matteo8p <matt8p@outlook.com>
2020-06-21 14:45:46 -0400

    vers

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const exec = require('@actions/exec');
 const path = require('path');
 const fs = require('fs');
 
+
 function main()
 {
+  const sql_directory = core.getInput('sql_directory');
   const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+
+  const srcPath = path.join(GITHUB_WORKSPACE, sql_directory);
+  console.log(sql_directory);
   console.log(GITHUB_WORKSPACE);
+  console.log(srcPath);
 }
 
 main();
