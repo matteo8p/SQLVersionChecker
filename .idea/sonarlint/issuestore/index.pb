68684e10a2d3e1070e765668a0e0ef9469c0059e
matteo8p <matt8p@outlook.com>
2020-06-21 14:33:30 -0400

    GITHUB_WORKSPACE

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+action.yml,3/1/311d8554708cd4816e36e38b191057ccdbc21722
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+8
+index.js,a/7/a77b15c0d64cf6111e025645e562faf8ccabb9d7
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = requre('@actions/exec');
+const path = require('path');
 const fs = require('fs');
 
-const nameToGreet = core.getInput('name');
-const repo = core.getInput('GITHUB_HEAD_REF');
-const master = core.getInput('GITHUB_BASE_REF')
+function main()
+{
+  const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+  console.log(GITHUB_WORKSPACE);
+}
 
-core.debug("Running V.1.1");
-console.log("Hola " + nameToGreet);
-console.log(repo.valueOf());
-console.log(master.valueOf());
+main();
 
 
 
